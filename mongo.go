786e1052a5bb6f779dbd1badada9e5dd6e2c8986
johnson <johnson@basics.fun>
2024-04-18 23:27:40 +0530

    added mogo db retry

--- mongo.go
+++ mongo.go
@@ -22,29 +22,13 @@ func main() {
 	// Set up MongoDB client options
 	clientOptions := options.Client().ApplyURI("mongodb://199.241.138.96:27017")
 
-	// Connect to MongoDBs
-	client, err := mongo.NewClient(clientOptions)
+	// Connect to MongoDB with retry
+	client, err := connectWithRetry(clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second) // Increase timeout to 30 seconds
-
-	defer cancel()
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Check the connection
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Connected to MongoDB!")
-
-	// Close the connection when the application exits
 	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
+		if err := client.Disconnect(context.Background()); err != nil {
 			log.Fatal(err)
 		}
 	}()
@@ -64,7 +48,7 @@ func main() {
 		}
 
 		// Insert user document into collection
-		_, err := collection.InsertOne(ctx, user)
+		_, err := collection.InsertOne(context.Background(), user)
 		if err != nil {
 			return err
 		}
@@ -79,14 +63,14 @@ func main() {
 		// Example query: find all users
 		filter := bson.M{}
 
-		cursor, err := collection.Find(ctx, filter)
+		cursor, err := collection.Find(context.Background(), filter)
 		if err != nil {
 			return err
 		}
-		defer cursor.Close(ctx)
+		defer cursor.Close(context.Background())
 
 		var users []User
-		if err := cursor.All(ctx, &users); err != nil {
+		if err := cursor.All(context.Background(), &users); err != nil {
 			return err
 		}
 
@@ -95,4 +79,40 @@ func main() {
 
 	// Start the server
 	log.Fatal(app.Listen(":3000"))
+}
+
+// connectWithRetry establishes connection to MongoDB with retry
+func connectWithRetry(clientOptions *options.ClientOptions) (*mongo.Client, error) {
+	var client *mongo.Client
+	var err error
+	for i := 0; i < 3; i++ { // Retry 3 times
+		client, err = mongo.NewClient(clientOptions)
+		if err != nil {
+			log.Println("Failed to create MongoDB client:", err)
+			time.Sleep(2 * time.Second) // Wait before retrying
+			continue
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second) // Increase timeout to 10 seconds
+		defer cancel()
+
+		err = client.Connect(ctx)
+		if err != nil {
+			log.Println("Failed to connect to MongoDB:", err)
+			time.Sleep(2 * time.Second) // Wait before retrying
+			continue
+		}
+
+		// Check the connection
+		err = client.Ping(ctx, nil)
+		if err != nil {
+			log.Println("Failed to ping MongoDB:", err)
+			time.Sleep(2 * time.Second) // Wait before retrying
+			continue
+		}
+
+		log.Println("Connected to MongoDB!")
+		return client, nil
+	}
+	return nil, err
 }
